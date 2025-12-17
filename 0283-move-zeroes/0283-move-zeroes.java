c72569fe83a123beadef60490364ac099e791b1a
AkshathaKarthikeyan-7 <akshatha.karthikeyank@gmail.com>
2025-12-17 21:50:17 +0530

    Time: 2 ms (87.27%), Space: 47.7 MB (38.17%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = -1;
+        int n = nums.length;
+
+        // find first zero
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
+        }
+        if(j == -1) return;   
+
+        int temp = 0;
+        for(int i = j+1; i < n; i++){
+            if(nums[i] != 0){
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
